56d54e5e6e0f8257c62e8c5772d8892c63ccb33c
Tongat Eko Prastiyo <82933215+ekoprastiyo@users.noreply.github.com>
2026-06-09 08:59:59 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ col1, col2 = st.columns([0.7, 0.3]) # Adjust ratios as needed
 with col1:
     st.write('Displaying the DataFrame:')
 with col2:
-    st.markdown(f":blue[Go to Spreadsheet](https://docs.google.com/spreadsheets/d/1SORCi_jXxEN-HSXWBOjX19FY8a6FzegPSAPbuh5k1sI/)", unsafe_allow_html=True)
+    st.markdown(f"[Go to Spreadsheet](https://docs.google.com/spreadsheets/d/1SORCi_jXxEN-HSXWBOjX19FY8a6FzegPSAPbuh5k1sI/)", unsafe_allow_html=True)
 
 # --- Sidebar for filters ---
 st.sidebar.header('Filter Options')
